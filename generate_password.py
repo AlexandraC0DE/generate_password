8a1661280b04b66f22ced27c50377af5e4035ff4
AlexandraC0DE <alexandramgarciag@gmail.com>
2023-04-18 22:48:49 -0500

    caracteres especiales y números

--- generate_password.py
+++ generate_password.py
@@ -2,7 +2,8 @@ import random
 import string
 
 def generate_password(length):
-    password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length))
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(password_characters) for i in range(length))
     return password
 
 print(generate_password(16))
